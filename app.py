bb6740924c1f581ec7cc7c0a7e67243487ad6b78
Abhijeeth Padarthi <rkinabhi@gmail.com>
2018-10-23 16:33:40 +0530

    init commit

---
+++ app.py
@@ -0,0 +1,86 @@
+import mysql.connector
+import csv
+from flask import Flask, request, jsonify, send_file
+
+app = Flask(__name__)
+host = "ingeniusdbinstance.ckikry8euhki.ap-south-1.rds.amazonaws.com"
+user = "ingenius2018"
+passwd = "Pesitbsc"
+database = "ingenius2018"
+
+@app.route("/")
+def root():
+    return "You have hit the root route"
+
+@app.route("/getTeams")
+def get_teams():
+    
+    mydb = mysql.connector.connect(host=host, user=user, passwd=passwd, database=database )
+    mycursor = mydb.cursor()
+
+    statement = "SELECT * FROM team" 
+    mycursor.execute(statement)
+    data = mycursor.fetchall()
+    
+    mycursor.close()
+    mydb.close()
+    return jsonify(data)
+
+@app.route("/getJudges")
+def get_judges():
+    mydb = mysql.connector.connect(host=host, user=user, passwd=passwd, database=database )
+    mycursor = mydb.cursor()
+
+    statement = "SELECT * FROM judge" 
+    mycursor.execute(statement)
+    data = mycursor.fetchall()
+    
+    mycursor.close()
+    mydb.close()
+    return jsonify(data)
+
+@app.route("/getScores")
+def get_scores():
+    mydb = mysql.connector.connect(host=host, user=user, passwd=passwd, database=database )
+    mycursor = mydb.cursor()
+
+    statement = "SELECT * FROM myScoreView" 
+    mycursor.execute(statement)
+    data = mycursor.fetchall()
+    
+    with open("scores.csv", "w", newline='') as f:
+        score_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        score_writer.writerow([i[0] for i in mycursor.description])
+        for row in list(data):
+            score_writer.writerow(row)
+    
+    mycursor.close()
+    mydb.close()
+
+    return send_file("scores.csv", attachment_filename="scores.csv", as_attachment=True)
+
+@app.route("/insertScore", methods=['POST'])
+def insert_score():
+    # TODO - Exception Handling
+    mydb = mysql.connector.connect(host=host, user=user, passwd=passwd, database=database )
+    mycursor = mydb.cursor()
+
+    insertion_statement = "INSERT INTO score VALUES (%s, %s, %s, %s, %s, %s)"
+    insertion_data = (
+            request.form.get("judgeID"), 
+            request.form.get("teamID"),
+            request.form.get("scoreOne"),
+            request.form.get("scoreTwo"),
+            request.form.get("scoreThree"),
+            request.form.get("scoreFour"),
+    )
+    mycursor.execute(insertion_statement, insertion_data)
+    mydb.commit()
+    
+    mycursor.close()
+    mydb.close()
+    return "Insertion successfull"
+
+
+if __name__ == "__main__":
+    app.run(host="192.168.2.6")
